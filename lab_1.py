e2569b7e64483d3787599522d7ecb01924eb9575
Zachary Klouchnikov <zachary.klou@gmail.com>
2026-01-13 16:28:16 -0500

    Transition to VS code

--- lab_1.py
+++ lab_1.py
@@ -1,3 +1,17 @@
 """
 IMPORTS
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+FUNCTIONS
+"""
+
+"""
+MAIN
+"""
+with open('data/bead_1_trial_1.txt', 'r') as file:
+        line = file.readlines()
+        print(line)
+
