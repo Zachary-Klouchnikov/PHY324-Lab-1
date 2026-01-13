e0fa6005e6b2bdc645a4f912cac987a464b7aa94
Zachary Klouchnikov <zachary.klou@gmail.com>
2026-01-13 16:55:02 -0500

    Created sample code for one .txt file

--- lab_1.py
+++ lab_1.py
@@ -4,6 +4,8 @@ IMPORTS
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rc('text', usetex = True)
+plt.rc('font', family = 'serif')
 """
 FUNCTIONS
 """
@@ -11,7 +13,22 @@ FUNCTIONS
 """
 MAIN
 """
-with open('data/bead_1_trial_1.txt', 'r') as file:
-        line = file.readlines()
-        print(line)
 
+time, position = np.loadtxt('data/bead_1_trial_2.txt', dtype = float, delimiter = '\t', skiprows = 2, unpack = True)
+
+"Plotting Total Absolute Frictional Force"
+plt.figure()
+
+# Plotting total absolute frictional force
+plt.scatter(time, position, color = 'teal', label = 'Position vs Time')
+
+# # Labels
+# plt.title("Total Absolute Frictional Force", fontsize = 12)
+# plt.xlabel("Velocity of the Mass (m/s)", fontsize = 12)
+# plt.ylabel("Force (N)", fontsize = 12)
+
+# plt.legend(fontsize = 12)
+# plt.grid()
+
+# plt.savefig('Figures\\Total Absolute Frictional Force.pdf')
+plt.show()
